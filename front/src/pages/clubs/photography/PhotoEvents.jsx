bd7d2317807dfa7473f8a095ed1fc938e7278dde
Kajal sain <kajal.sain0905@gmail.com>
2025-06-14 20:14:08 +0530

    Update PhotoEvents.jsx

--- front/src/pages/clubs/photography/PhotoEvents.jsx
+++ front/src/pages/clubs/photography/PhotoEvents.jsx
@@ -10,7 +10,7 @@ export default function PhotoEvents(){
     const [Events,setEvents] = useState([])
      const handleClick = async()=>{
         try{
-       const res1 = await axios.get('http://localhost:3000/login/getAllEvents',{
+       const res1 = await axios.get('https://clubsphere-production.up.railway.app/login/getAllEvents',{
         withCredentials:'true',
         params:{
             clubName:'photography'
@@ -21,7 +21,7 @@ export default function PhotoEvents(){
     }
     catch(err1){
         try{
-  const res2= await axios.get('http://localhost:3000/loginExecutive/getAllEvents',{
+  const res2= await axios.get('https://clubsphere-production.up.railway.app/loginExecutive/getAllEvents',{
         withCredentials:'true',
         params:{
             clubName:'photography'
